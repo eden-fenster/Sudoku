86be3b132fd79ada44f5a3a023e11b92d4957e0a
eden-fenster <eden.fenster@gmail.com>
2023-04-23 11:19:52 +0300

    Update

--- sudoku/scratch_3.py
+++ sudoku/scratch_3.py
@@ -1,6 +1,6 @@
 import logging
 import sys
-from typing import List
+from typing import List, Tuple
 
 from flask import Flask, request, render_template
 from flask_restful import Resource, Api
@@ -12,12 +12,22 @@ app = Flask(__name__)
 # creating an API object
 api = Api(app)
 
+
 # For GET request to http://localhost:5000/
 class GetFromUser(Resource):
     def get(self):
         return render_template('upload.html')
+
     def post(self):
         return {'data': 'posted'}
+
+
+def get_solutions(initial_grid: List[List[int]]) -> Tuple[List[List[int]]]:
+    solutions, have_solution, information = sudoku.solve_sudoku(grid=initial_grid)
+    _ = have_solution
+    _ = information
+    return solutions
+
 
 # For Post request to http://localhost:5000/results
 class PrintResults(Resource):
@@ -30,15 +40,14 @@ class PrintResults(Resource):
             sys.exit(1)
         initial_grid: List[List[int]] = sudoku.create_sudoku(read_file)
         initial_grid_string = sudoku.print_grid(description="Initial grid", grid=initial_grid)
-        solutions, have_solution, information = sudoku.solve_sudoku(grid=initial_grid)
+        solutions = get_solutions(initial_grid=initial_grid)
         solved_grid_string: str = ''
         for i, solution in enumerate(solutions):
             solved_grid_string += sudoku.print_grid(description=f"solution {i + 1}", grid=solution)
 
-        dict_sudoku: dict = {{"The initial grid: <br>": initial_grid_string},
-                           {"<br>The solved grid: <br>" + solved_grid_string}}
+        dict_sudoku = {{"The initial grid: <br>": initial_grid_string},
+                       {"<br>The solved grid: <br>" + solved_grid_string}}
         return dict_sudoku
-
 
 
 api.add_resource(GetFromUser, '/')
