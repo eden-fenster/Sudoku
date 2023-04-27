6dfc9ba088998873a3d29236448c35b62226b0e4
eden-fenster <59683043+eden-fenster@users.noreply.github.com>
2023-04-27 12:57:11 +0300

    * Hope this works.

--- web/app.py
+++ web/app.py
@@ -1,34 +1,43 @@
 #!/usr/bin/env python3
 import logging
 import sys
-from typing import List
+from typing import List, Tuple
+
+from flask import Flask, request, render_template, jsonify
+from flask_restful import Resource, Api
 from processor.app import GetResults
 
+import sudoku
 
-from flask import *
-
-import web.sudoku
-
+# create an instance of flask
 app = Flask(__name__)
+# creating an API object
+api = Api(app)
 
 
+# For GET request to http://localhost:5000/
 @app.route('/')
 def upload_file():
     return render_template('upload.html')
 
 
-@app.route('/results', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
+
+# For Post request to http://localhost:5000/results
+class PrintResults(Resource):
+    def post(self):
         f = request.files['file']
         f.save(f.filename)
-        read_file: List[str] = web.sudoku.read_file(file_to_open=f.filename)
+        read_file: List[str] = sudoku.read_file(file_to_open=f.filename)
         if not read_file:
             logging.error(f"No sudoku found")
             sys.exit(1)
-        initial_grid: List[List[int]] = web.sudoku.create_sudoku(read_file)
+        initial_grid: List[List[int]] = sudoku.create_sudoku(read_file)
         return GetResults.get(initial_grid=initial_grid)
 
 
+
+api.add_resource(PrintResults, '/results')
+
+#
 if __name__ == '__main__':
     app.run(debug=True)
