e314b9a99db342ff64374e51fb2a8e9f7bc89bee
eden-fenster <59683043+eden-fenster@users.noreply.github.com>
2023-05-01 11:08:44 +0300

    * Trying to figure out how to input parameters using postman, can use postman for simpler stuff.

--- sudoku/big_brain_moment.py
+++ sudoku/big_brain_moment.py
@@ -1,3 +1,5 @@
+import logging
+
 from flask import Flask, jsonify, request
 import sudoku
 app = Flask(__name__)
@@ -18,11 +20,10 @@ def add_grids():
     initial_grid = grids[0]["Grid"]
     initial_string: str = sudoku.print_grid(description="Initial grid", grid=initial_grid)
     solved = sudoku.get_solutions(initial_grid=initial_grid)
-    print(solved)
     solved_string: str = ''
     for i, solve in enumerate(solved):
-        solved_string += sudoku.print_grid(description=f"solution {i + 1}", grid=solved)
-    grid_strings.append({"Initial Grid": initial_string, "Solved Grid": solved_string})
+        solved_string += sudoku.print_grid(description=f"solution {i + 1}", grid=solve)
+    grid_strings.append("Initial Grid: <br>" + initial_string + "<br>Solved Grid: <br>" + solved_string)
     return '', 204
 
 if __name__ == '__main__':
