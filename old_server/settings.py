938c8ccb9ed03e84534f53196537667d416b5a34
eden-fenster <59683043+eden-fenster@users.noreply.github.com>
2023-05-04 09:50:38 +0300

    * Need to make webpage look prettier. * Minimize content in each container - mostly there. * Add documentation. * Figure out why it doesn't work with 3x3 or 2x2 boards. * Assign IPs in compose. * Create network subnet. * Use DNS name for code.

--- old_server/settings.py
+++
@@ -1,4 +0,0 @@
-SECRET_KEY = 'its-sudoku-time'
-DEBUG = True
-MONGODB_HOST = 'mongodb'
-MONGODB_DB = 'sudoku_api'
--- unused_files/another_server.py
+++
@@ -1,8 +0,0 @@
-from server_template import Server
-
-
-DEFAULT_PATH = "index.html"
-
-
-class SudokuServer(Server):
-    super().do_GET(path=DEFAULT_PATH)
