5d28d33c6dd464fd3413557b369fb1ba6c34b9a0
eden-fenster <eden.fenster@gmail.com>
2023-05-12 08:35:59 +0300

    * Changed syntax. * Need to have logs of each operation inside volume.

--- web/app.py
+++ web/app.py
@@ -22,7 +22,7 @@ def post():
     # Gets the file and saves it.
     f = request.files['file']
     # If empty, return.
-    if f.filename is '':
+    if f.filename == '':
         return render_template('style.html') + render_template_string("No Grid has been provided")
     f.save(f.filename)
     logging.debug(f"The file is {f}")
