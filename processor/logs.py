a496cfdcb11a7327d7772540b9a09056e9091b6c
eden-fenster <eden.fenster@gmail.com>
2023-05-17 10:38:45 +0300

    * File works on it's own, but won't write to volume in the container for some reason.

--- processor/logs.py
+++ processor/logs.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 
-with open("output.txt", "w") as f:
+with open("/path/to/output.txt", "w") as f:
     print("Hello stackoverflow!", file=f)
     print("I have a question.", file=f)
