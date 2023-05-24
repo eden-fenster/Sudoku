f2c36bfec51023ae1e40ab39d4be25ff31dd5cdb
eden-fenster <eden.fenster@gmail.com>
2023-05-24 09:38:05 +0300

    * All python files get 10/10 on pylint except for processor/sudoku_class.

--- database/create_database.py
+++ database/create_database.py
@@ -4,9 +4,9 @@ import sqlite3
 
 conn = sqlite3.connect('sudoku_results.db')
 c = conn.cursor()
-command = """CREATE TABLE IF NOT EXISTS sudoku_results (
+COMMAND = """CREATE TABLE IF NOT EXISTS sudoku_results (
 results text
 )"""
-c.execute(command)
+c.execute(COMMAND)
 conn.commit()
 conn.close()
