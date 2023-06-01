cc92ad90a0732b36aa2f889e06bfd3420d2a2a94
eden-fenster <eden.fenster@gmail.com>
2023-06-01 19:44:43 +0300

    * Printing out database in separate container. * Not resetting from previous run for some reason. * Need to query database.

--- query/app.py
+++ query/app.py
@@ -1,49 +1,19 @@
 #!/usr/bin/env python3
 """Sudoku Solver - Query Database"""
-
-from flask import Flask
+import requests
+from flask import Flask, render_template, render_template_string
 
 app = Flask(__name__)
 
+
+# return the database.
 @app.route('/')
-def get():
-    """Return somethings"""
-    return "Hello World"
-# # List to store received response.
-# responses = []
-#
-#
-#
-# # Returns the results
-# @app.route('/database')
-# def get_database():
-#     """Getting from database"""
-#     return json.dumps(sudoku_db.show_all())
-#
-#
-# @app.route('/database', methods=['POST'])
-# def add_to_database():
-#     """Adding to database"""
-#     # Adding to list.
-#     responses.append(request.get_json())
-#     time = responses[len(responses) - 1]["Time"]
-#     date = responses[len(responses) - 1]["Date"]
-#     # Create database.
-#     create(database_name='sudoku_results')
-#     logging.debug("created")
-#     # Adding to database
-#     sudoku_db.add_one(time=time, date=date)
-#     return '', 204
-#
-#
-# # Delete previous records.
-# @app.route('/database', methods=['DELETE'])
-# def delete_records():
-#     """Deleting from database"""
-#     if len(responses) > 1:
-#         sudoku_db.delete_one(id='1')
-#         logging.debug("record deleted")
-#     return '', 204
+def get_database():
+    """Return our database"""
+    response = requests.get('http://sudoku_database:3000/database')
+    return render_template('results.html', Title='Database') + render_template_string(response.json())
+
+# TODO: Query database between dates.
 
 
 if __name__ == '__main__':
