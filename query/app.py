79ec2cc26d2fe7a3c5186257028e8cb151692c5b
eden-fenster <eden.fenster@gmail.com>
2023-08-07 20:20:17 +0300

    SPS-19* I now have links working thanks to changing the etc/host file. * Need to find another way to do so. * Need to add different colored backgrounds for each element.

--- query/app.py
+++ query/app.py
@@ -63,4 +63,4 @@ def return_all():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=1000, host='sudoku_query', ssl_context='adhoc')
+    app.run(debug=True, port=1000, host='sudoku_query')
